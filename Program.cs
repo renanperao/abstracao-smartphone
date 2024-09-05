471b93faf66731d95d50e8aae119365b691002c6
Renan Perão <peraorenan@gmail.com>
2024-09-05 17:28:27 -0300

    implementando-funcoes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia meuNokia = new Nokia("123456789", "Nokia 3310", "111222333444", 32);
+        
+        meuNokia.Ligar();
+        meuNokia.InstalarAplicativo("WhatsApp"); 
+
+        Iphone novoIphone = new Iphone("41992795999", "Iphone Xr", "1234567890", 128);
+
+        novoIphone.Ligar();
+        novoIphone.InstalarAplicativo("Clash Royale");
+    }
+}
